3240932d36798d57fd5ce306dea5c1418f87c401
Nick Verlinde <nick.verlinde@gmail.com>
2024-07-19 17:39:34 +0200

    sf

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,3 +1,6 @@
 {{ if ne .chezmoi.os "linux" }}
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh >install.sh
+chmod +x install.sh
+./install.sh -f -b ~/bin/
+rm install.sh
 {{ end }}
