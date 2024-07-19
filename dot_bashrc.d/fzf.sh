b3bbc9348bc7bf1fb819ccd92e70ab0a5bbeaa8e
Nick Verlinde <nick.verlinde@gmail.com>
2024-07-19 18:16:00 +0200

    sdf

--- dot_bashrc.d/fzf.sh
+++ dot_bashrc.d/fzf.sh
@@ -1,17 +1,17 @@
 # add if [[ "$OSTYPE" == darwin* ]]; then to case statement
-case "$OSTYPE" in
-"linux-gnu")
-	. /usr/share/fzf/shell/key-bindings.bash
-	;;
-darwin*)
-	if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-		PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
-	fi
-	[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2>/dev/null
-	source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
-	;;
-*)
-	echo "no fzf"
-	;;
-esac
-
+eval "$(fzf --bash)"
+#case "$OSTYPE" in
+#"linux-gnu")
+#	. /usr/share/fzf/shell/key-bindings.bash
+#	;;
+#darwin*)
+#	if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+#		PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+#	fi
+#	[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2>/dev/null
+#	source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+#	;;
+#*)
+#	echo "no fzf"
+#	;;
+#esac
